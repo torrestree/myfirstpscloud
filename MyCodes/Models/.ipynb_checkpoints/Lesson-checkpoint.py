c94599bdca6080eacc407183a00b3d1a6a96f54a
torrestree <drizzt.tree@hotmail.com>
2018-12-07 12:40:42 +0000

    12

--- MyCodes/Models/.ipynb_checkpoints/Lesson-checkpoint.py
+++ MyCodes/Models/.ipynb_checkpoints/Lesson-checkpoint.py
@@ -1,6 +1,6 @@
 from odoo import api, fields, models
 
-class Lesson:Name='MyCodes.Models.Lesson'
+class Lesson(models.Model):Name='MyCodes.Models.Lesson'
     Name=fields.Char(string='Name')
     TeacherID=fields.Many2one('TrainingCodes.Person',string='老师')
     StartDate=fields.Date(string='开始日期')
